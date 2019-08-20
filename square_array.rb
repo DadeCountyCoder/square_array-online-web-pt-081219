430dc60081928060c51813ec617e4c6628c4d9e0
Alfredo Diaz <mail4alfredo@gmail.com>
2019-08-20 18:17:18 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,12 +1,12 @@
 
-def square_array(numbers)
+def square_array(array)
   new_array=[]
   array.each do |numbers|
-    new_array = [numbers** 2]
+    new_array << (numbers** 2)
   end
   return new_array
 end
 
-numbers = [1,2,3]
+
 square_array(numbers)
  
