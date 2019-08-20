c2762623f4facc233b3b06ace807756ce0daf620
Alfredo Diaz <mail4alfredo@gmail.com>
2019-08-20 17:54:30 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,6 @@
 numbers = [1,2,3]
-def square_array(numbers)
-  numbers.each do |numbers|
+def square_array(array)
+  array.each do |numbers|
     new_array = [numbers ** 2]
     puts new_array
   end
